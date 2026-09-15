a3ca4372b83168dd63f015a320d117a31c2d3bbb
Wajih BENABDESSALEM <wajih.abdes@gmail.com>
2026-09-15 15:16:05 +0200

    Initial Commit

---
+++ AI Notes iOS/AI_Notes_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Notes_iOSApp.swift
+//  AI Notes iOS
+//
+//  Created by Wajih ABDESSALEM on 15/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct AI_Notes_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI Notes iOSTests/AI_Notes_iOSTests.swift
@@ -0,0 +1,19 @@
+//
+//  AI_Notes_iOSTests.swift
+//  AI Notes iOSTests
+//
+//  Created by Wajih ABDESSALEM on 15/09/2026.
+//
+
+import Testing
+@testable import AI_Notes_iOS
+
+struct AI_Notes_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
